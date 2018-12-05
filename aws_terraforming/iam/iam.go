fdef3e765a75991ce4f850d9fecc152d20f56205
Sergey Lanzman <sergeylanz@google.com>
2018-12-06 01:07:58 +0200

    aws: add iam
    Change-Id: If1cdc32ba45bd229662f2724274a8f3178ee2eff

--- aws_terraforming/iam/iam.go
+++ aws_terraforming/iam/iam.go
@@ -1,6 +1,7 @@
 package iam
 
 import (
+	"fmt"
 	"strings"
 
 	"waze/terraform/aws_terraforming/aws_generator"
@@ -14,7 +15,7 @@ import (
 
 var ignoreKey = map[string]bool{
 	"^id$":       true,
-	"^arn":       true,
+	"^arn$":      true,
 	"^unique_id": true,
 }
 
@@ -43,12 +44,12 @@ func (g IamGenerator) Generate(region string) ([]terraform_utils.TerraformResour
 		return []terraform_utils.TerraformResource{}, map[string]terraform_utils.ResourceMetaData{}, err
 	}
 	//TODO ALL
-	/*svc.ListGroupsPages()
-	svc.ListPoliciesPages()
-	svc.ListRolesPages()
-	svc.ListAccessKeysPages()
-	svc.ListGroupPoliciesPages()
-	svc.ListGroupsPages()*/
+	err = g.getGroups(svc)
+	err = g.getPolicies(svc)
+	/*
+		svc.ListRolesPages()
+		svc.ListAccessKeysPages()
+		svc.ListGroupPoliciesPages()*/
 	return g.resources, g.metadata, nil
 }
 
@@ -71,7 +72,8 @@ func (g *IamGenerator) getUsers(svc *iam.IAM) error {
 			}
 
 			g.getUserPolices(svc, user.UserName)
-			g.getUserGroup(svc, user.UserName)
+			//g.getUserGroup(svc, user.UserName) //not work maybe terraform-aws bug
+			break
 		}
 
 		return !lastPage
@@ -79,8 +81,8 @@ func (g *IamGenerator) getUsers(svc *iam.IAM) error {
 	return err
 }
 
-func (g *IamGenerator) getUserGroup(svc *iam.IAM, userName *string) {
-	svc.ListGroupsForUserPages(&iam.ListGroupsForUserInput{UserName: userName}, func(userGroup *iam.ListGroupsForUserOutput, lastPage bool) bool {
+func (g *IamGenerator) getUserGroup(svc *iam.IAM, userName *string) error {
+	err := svc.ListGroupsForUserPages(&iam.ListGroupsForUserInput{UserName: userName}, func(userGroup *iam.ListGroupsForUserOutput, lastPage bool) bool {
 		for _, group := range userGroup.Groups {
 			resourceName := aws.StringValue(group.GroupName)
 			groupIDAttachment := aws.StringValue(group.GroupName)
@@ -89,8 +91,8 @@ func (g *IamGenerator) getUserGroup(svc *iam.IAM, userName *string) {
 				resourceName,
 				"aws_iam_user_group_membership",
 				"aws",
-				map[string]interface{}{"user": userName},
 				map[string]string{},
+				map[string]string{"user": aws.StringValue(userName)},
 			))
 			g.metadata[groupIDAttachment] = terraform_utils.ResourceMetaData{
 				Provider:         "aws",
@@ -101,10 +103,11 @@ func (g *IamGenerator) getUserGroup(svc *iam.IAM, userName *string) {
 		}
 		return !lastPage
 	})
+	return err
 }
 
-func (g *IamGenerator) getUserPolices(svc *iam.IAM, userName *string) {
-	svc.ListUserPoliciesPages(&iam.ListUserPoliciesInput{UserName: userName}, func(userPolices *iam.ListUserPoliciesOutput, lastPage bool) bool {
+func (g *IamGenerator) getUserPolices(svc *iam.IAM, userName *string) error {
+	err := svc.ListUserPoliciesPages(&iam.ListUserPoliciesInput{UserName: userName}, func(userPolices *iam.ListUserPoliciesOutput, lastPage bool) bool {
 		for _, policy := range userPolices.PolicyNames {
 			resourceName := strings.Replace(aws.StringValue(policy), "@", "", -1)
 			policyID := aws.StringValue(userName) + ":" + aws.StringValue(policy)
@@ -121,7 +124,95 @@ func (g *IamGenerator) getUserPolices(svc *iam.IAM, userName *string) {
 				AllowEmptyValue:  allowEmptyValues,
 				AdditionalFields: map[string]string{},
 			}
+
 		}
 		return !lastPage
 	})
+	return err
+}
+
+func (g *IamGenerator) getPolicies(svc *iam.IAM) error {
+	err := svc.ListPoliciesPages(&iam.ListPoliciesInput{}, func(policies *iam.ListPoliciesOutput, lastPage bool) bool {
+		for _, policy := range policies.Policies {
+			resourceName := aws.StringValue(policy.PolicyName)
+			policyARN := aws.StringValue(policy.Arn)
+			// not use AWS main policy
+			//if strings.HasPrefix(policyARN, "arn:aws:iam::aws:policy") {
+			//	continue
+			//}
+			g.resources = append(g.resources, terraform_utils.NewTerraformResource(
+				policyARN,
+				resourceName,
+				"aws_iam_policy",
+				"aws",
+				nil,
+				map[string]string{}))
+			g.metadata[resourceName] = terraform_utils.ResourceMetaData{
+				Provider:         "aws",
+				IgnoreKeys:       ignoreKey,
+				AllowEmptyValue:  allowEmptyValues,
+				AdditionalFields: map[string]string{},
+			}
+			g.resources = append(g.resources, terraform_utils.NewTerraformResource(
+				policyARN,
+				resourceName,
+				"aws_iam_policy_attachment",
+				"aws",
+				nil,
+				map[string]string{
+					"policy_arn": policyARN,
+					"name":       resourceName,
+				}))
+			break
+
+		}
+		return !lastPage
+	})
+	return err
+}
+
+func (g *IamGenerator) getGroups(svc *iam.IAM) error {
+	err := svc.ListGroupsPages(&iam.ListGroupsInput{}, func(groups *iam.ListGroupsOutput, lastPage bool) bool {
+		for _, group := range groups.Groups {
+			resourceName := aws.StringValue(group.GroupName)
+			g.resources = append(g.resources, terraform_utils.NewTerraformResource(
+				resourceName,
+				resourceName,
+				"aws_iam_group",
+				"aws",
+				nil,
+				map[string]string{}))
+			g.metadata[resourceName] = terraform_utils.ResourceMetaData{
+				Provider:         "aws",
+				IgnoreKeys:       ignoreKey,
+				AllowEmptyValue:  allowEmptyValues,
+				AdditionalFields: map[string]string{},
+			}
+			g.resources = append(g.resources, terraform_utils.NewTerraformResource(
+				resourceName,
+				resourceName,
+				"aws_iam_group_membership",
+				"aws",
+				nil,
+				map[string]string{
+					"group": resourceName,
+					"name":  resourceName,
+				}))
+		}
+		return !lastPage
+	})
+	return err
+}
+
+// PostGenerateHook for add policy json as heredoc
+func (IamGenerator) PostGenerateHook(resources []terraform_utils.TerraformResource) ([]terraform_utils.TerraformResource, error) {
+	for _, resource := range resources {
+		if resource.ResourceType == "aws_iam_policy" || resource.ResourceType == "aws_iam_user_policy" {
+			policy := resource.Item.(interface{}).(map[string]interface{})["policy"].(string)
+			resource.Item.(interface{}).(map[string]interface{})["policy"] = fmt.Sprintf(`<<POLICY
+%s
+POLICY`, policy)
+		}
+	}
+	return resources, nil
 }
