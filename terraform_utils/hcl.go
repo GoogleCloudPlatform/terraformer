fc03818e6b4a8d9716bde4c87614700f261a5e0b
Sergey Lanzman <sergeylanz@google.com>
2018-12-09 16:47:18 +0200

    Add LICENSE

--- terraform_utils/hcl.go
+++ terraform_utils/hcl.go
@@ -1,3 +1,17 @@
+// Copyright 2018 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package terraform_utils
 
 import (
@@ -11,6 +25,8 @@ import (
 	hcl_printer "github.com/hashicorp/hcl/hcl/printer"
 	hcl_parcer "github.com/hashicorp/hcl/json/parser"
 )
+
+// Copy code from https://github.com/kubernetes/kops project with few changes for support many provider and heredoc
 
 const safeChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
 
--- terraform_utils/utils.go
+++ terraform_utils/utils.go
@@ -1,3 +1,17 @@
+// Copyright 2018 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package terraform_utils
 
 import (
@@ -28,11 +42,10 @@ func GenerateTfState(resources []TerraformResource) error {
 		return err
 	}
 	// use plugins from os.Getenv("HOME") + "/.terraform.d"
-	meta := command.Meta{
+	c := command.RefreshCommand{Meta: command.Meta{
 		OverrideDataDir: os.Getenv("HOME") + "/.terraform.d",
 		Ui:              cli.Ui(&cli.ConcurrentUi{Ui: &cli.BasicUi{Writer: os.Stdout}}),
-	}
-	c := command.RefreshCommand{Meta: meta}
+	}}
 	path, _ := os.Getwd()
 	mod, _ := c.Module(path)
 
@@ -49,8 +62,8 @@ func GenerateTfState(resources []TerraformResource) error {
 	}
 
 	opReq := c.Operation()
-	opReq.Type = backend.OperationTypeRefresh
 	opReq.Module = mod
+	opReq.Type = backend.OperationTypeRefresh
 	op, err := c.RunOperation(b, opReq)
 	if err != nil {
 		return err
