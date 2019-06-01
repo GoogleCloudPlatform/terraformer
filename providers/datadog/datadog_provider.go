239ee2b9d2175f02be695566365b410cb9a9b133
kterada0509 <kterada.0509sg@gmail.com>
2019-06-02 00:30:58 +0900

    Add support datadog_synthetics_test resource

--- providers/datadog/datadog_provider.go
+++ providers/datadog/datadog_provider.go
@@ -86,9 +86,10 @@ func (p *DatadogProvider) InitService(serviceName string) error {
 // GetSupportedService return map of support service for Datadog
 func (p *DatadogProvider) GetSupportedService() map[string]terraform_utils.ServiceGenerator {
 	return map[string]terraform_utils.ServiceGenerator{
-		"downtime": &DowntimeGenerator{},
-		"monitor":  &MonitorGenerator{},
-		"user":     &UserGenerator{},
+		"downtime":   &DowntimeGenerator{},
+		"monitor":    &MonitorGenerator{},
+		"synthetics": &SyntheticsGenerator{},
+		"user":       &UserGenerator{},
 	}
 }
 
