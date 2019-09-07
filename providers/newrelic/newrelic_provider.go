bb52f3ed36675d7764fcb1fd05e363367947f638
quan.hoang <hdquan@protonmail.com>
2019-09-07 20:16:48 +0700

    Add NewRelic provider

---
+++ providers/newrelic/newrelic_provider.go
@@ -0,0 +1,70 @@
+// Copyright 2019 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package newrelic
+
+import (
+	"errors"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraform_utils"
+)
+
+const newrelicProviderVersion = "~> 1.5"
+
+type NewRelicProvider struct {
+	terraform_utils.Provider
+}
+
+func (p *NewRelicProvider) Init(args []string) error {
+	return nil
+}
+
+func (p *NewRelicProvider) GetName() string {
+	return "newrelic"
+}
+
+func (p *NewRelicProvider) GetProviderData(arg ...string) map[string]interface{} {
+	return map[string]interface{}{
+		"provider": map[string]interface{}{
+			"newrelic": map[string]interface{}{
+				"version": newrelicProviderVersion,
+			},
+		},
+	}
+}
+
+func (NewRelicProvider) GetResourceConnections() map[string]map[string][]string {
+	return map[string]map[string][]string{}
+}
+
+func (p *NewRelicProvider) GetSupportedService() map[string]terraform_utils.ServiceGenerator {
+	return map[string]terraform_utils.ServiceGenerator{
+		"alert":      &AlertGenerator{},
+		"infra":      &InfraGenerator{},
+		"synthetics": &SyntheticsGenerator{},
+		"dashboard":  &DashboardGenerator{},
+	}
+}
+
+func (p *NewRelicProvider) InitService(serviceName string) error {
+	var isSupported bool
+	if _, isSupported = p.GetSupportedService()[serviceName]; !isSupported {
+		return errors.New("newrelic: " + serviceName + " not supported service")
+	}
+	p.Service = p.GetSupportedService()[serviceName]
+	p.Service.SetName(serviceName)
+	p.Service.SetProviderName(p.GetName())
+
+	return nil
+}
