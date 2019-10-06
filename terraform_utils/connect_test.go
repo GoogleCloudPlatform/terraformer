5751af9e2f30c6b6b686a164bc030e3b6a89e846
Patryk Orwat <patrykorwat@gmail.com>
2019-10-06 16:26:39 +0700

    refactoring resource connections to add references in nested attributed

--- terraform_utils/connect_test.go
+++ terraform_utils/connect_test.go
@@ -19,116 +19,130 @@ import (
 	"testing"
 )
 
+func TestSimpleReference(t *testing.T) {
+	importResources := map[string][]Resource{
+		"type1": {prepare("ID1", "type1", map[string]string{
+			"type2_ref": "ID2",
+		})},
+		"type2": {prepare("ID2", "type2", map[string]string{})},
+	}
+
+	resourceConnections :=  map[string]map[string][]string{
+		"type1": {
+			"type2": {"type2_ref", "id"},
+		},
+	}
+	resources := ConnectServices(importResources, resourceConnections)
+
+	if !reflect.DeepEqual(resources, map[string][]Resource{
+		"type1": {prepare("ID1", "type1", map[string]string{
+			"type2_ref": "${data.terraform_remote_state.type2.outputs.type2_name-type2_id}",
+		})},
+		"type2": {prepare("ID2", "type2", map[string]string{})},
+	}) {
+		t.Errorf("failed to connect %v", resources)
+	}
+}
+
+func TestManyReferences(t *testing.T) {
+	importResources := map[string][]Resource{
+		"type1": {prepare("ID1", "type1", map[string]string{
+			"type2_ref1": "ID2",
+			"type2_ref2": "ID2",
+		})},
+		"type2": {prepare("ID2", "type2", map[string]string{})},
+	}
+
+	resourceConnections := map[string]map[string][]string{
+		"type1": {
+			"type2": {
+				"type2_ref1", "id",
+				"type2_ref2", "id",
+			},
+		},
+	}
+	resources := ConnectServices(importResources, resourceConnections)
+
+	if !reflect.DeepEqual(resources, map[string][]Resource{
+		"type1": {prepare("ID1", "type1", map[string]string{
+			"type2_ref1": "${data.terraform_remote_state.type2.outputs.type2_name-type2_id}",
+			"type2_ref2": "${data.terraform_remote_state.type2.outputs.type2_name-type2_id}",
+		})},
+		"type2": {prepare("ID2", "type2", map[string]string{})},
+	}) {
+		t.Errorf("failed to connect %v", resources)
+	}
+}
+
+func TestResourceGroups(t *testing.T) {
+	importResources := map[string][]Resource{
+		"group1": {prepare("ID1", "type1", map[string]string{
+			"type2_ref1": "ID2",
+			"type2_ref2": "ID2",
+		}),
+			prepare("ID3", "type3", map[string]string{})},
+		"group2": {
+			prepare("ID2", "type2", map[string]string{
+				"uid": "ID2",
+			}),
+			prepare("ID4", "type4", map[string]string{})},
+	}
+
+	resourceConnections := map[string]map[string][]string{
+		"group1": {
+			"group2": {
+				"type2_ref1", "uid",
+				"type2_ref2", "uid",
+			},
+		},
+	}
+	resources := ConnectServices(importResources, resourceConnections)
+
+	if !reflect.DeepEqual(resources, map[string][]Resource{
+		"group1": {prepare("ID1", "type1", map[string]string{
+			"type2_ref1": "${data.terraform_remote_state.group2.outputs.type2_name-type2_uid}",
+			"type2_ref2": "${data.terraform_remote_state.group2.outputs.type2_name-type2_uid}",
+		}),
+			prepare("ID3", "type3", map[string]string{}),
+		},
+		"group2": {
+			prepare("ID2", "type2", map[string]string{
+				"uid": "ID2",
+			}),
+			prepare("ID4", "type4", map[string]string{})},
+	}) {
+		t.Errorf("failed to connect %v", resources)
+	}
+}
+
+func TestNestedReference(t *testing.T) {
+	importResources := map[string][]Resource{
+		"type1": {prepare("ID1", "type1", map[string]string{
+			"nested.type2_ref": "ID2",
+		})},
+		"type2": {prepare("ID2", "type2", map[string]string{})},
+	}
+
+	resourceConnections := map[string]map[string][]string{
+		"type1": {
+			"type2": {"nested.type2_ref", "id"},
+		},
+	}
+	resources := ConnectServices(importResources, resourceConnections)
+
+	if !reflect.DeepEqual(resources, map[string][]Resource{
+		"type1": {prepare("ID1", "type1", map[string]string{
+			"nested.type2_ref": "${data.terraform_remote_state.type2.outputs.type2_name-type2_id}",
+		})},
+		"type2": {prepare("ID2", "type2", map[string]string{})},
+	}) {
+		t.Errorf("failed to connect %v", resources)
+	}
+}
+
 func prepare(ID, resourceType string, attributes map[string]string) Resource {
 	r := NewResource(ID, "name-"+resourceType, resourceType, "provider", attributes, []string{}, map[string]interface{}{})
 	r.InstanceState.Attributes["id"] = r.InstanceState.ID
 	r.ConvertTFstate()
 	return r
-}
-
-func TestConnectServices(t *testing.T) {
-	type args struct {
-		importResources     map[string][]Resource
-		resourceConnections map[string]map[string][]string
-	}
-	tests := []struct {
-		name string
-		args args
-		want map[string][]Resource
-	}{
-		{
-			name: "simple test",
-			args: args{
-				importResources: map[string][]Resource{
-					"type1": {prepare("ID1", "type1", map[string]string{
-						"type2_ref": "ID2",
-					})},
-					"type2": {prepare("ID2", "type2", map[string]string{})},
-				},
-				resourceConnections: map[string]map[string][]string{
-					"type1": {
-						"type2": {"type2_ref", "id"},
-					},
-				},
-			},
-			want: map[string][]Resource{
-				"type1": {prepare("ID1", "type1", map[string]string{
-					"type2_ref": "${data.terraform_remote_state.type2.outputs.type2_name-type2_id}",
-				})},
-				"type2": {prepare("ID2", "type2", map[string]string{})},
-			},
-		},
-		{
-			name: "many refs test",
-			args: args{
-				importResources: map[string][]Resource{
-					"type1": {prepare("ID1", "type1", map[string]string{
-						"type2_ref1": "ID2",
-						"type2_ref2": "ID2",
-					})},
-					"type2": {prepare("ID2", "type2", map[string]string{})},
-				},
-				resourceConnections: map[string]map[string][]string{
-					"type1": {
-						"type2": {
-							"type2_ref1", "id",
-							"type2_ref2", "id",
-						},
-					},
-				},
-			},
-			want: map[string][]Resource{
-				"type1": {prepare("ID1", "type1", map[string]string{
-					"type2_ref1": "${data.terraform_remote_state.type2.outputs.type2_name-type2_id}",
-					"type2_ref2": "${data.terraform_remote_state.type2.outputs.type2_name-type2_id}",
-				})},
-				"type2": {prepare("ID2", "type2", map[string]string{})},
-			},
-		},
-		{
-			name: "ref groups test",
-			args: args{
-				importResources: map[string][]Resource{
-					"group1": {prepare("ID1", "type1", map[string]string{
-						"type2_ref1": "ID2",
-						"type2_ref2": "ID2",
-					}),
-						prepare("ID3", "type3", map[string]string{})},
-					"group2": {
-						prepare("ID2", "type2", map[string]string{
-							"uid": "ID2",
-						}),
-						prepare("ID4", "type4", map[string]string{})},
-				},
-				resourceConnections: map[string]map[string][]string{
-					"group1": {
-						"group2": {
-							"type2_ref1", "uid",
-							"type2_ref2", "uid",
-						},
-					},
-				},
-			},
-			want: map[string][]Resource{
-				"group1": {prepare("ID1", "type1", map[string]string{
-					"type2_ref1": "${data.terraform_remote_state.group2.outputs.type2_name-type2_uid}",
-					"type2_ref2": "${data.terraform_remote_state.group2.outputs.type2_name-type2_uid}",
-				}),
-					prepare("ID3", "type3", map[string]string{}),
-				},
-				"group2": {
-					prepare("ID2", "type2", map[string]string{
-						"uid": "ID2",
-					}),
-					prepare("ID4", "type4", map[string]string{})},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ConnectServices(tt.args.importResources, tt.args.resourceConnections); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ConnectServices() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
