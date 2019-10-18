db1fc953e957ed5da68e73675e01422575589026
Allison Doami <adoami@chanzuckerberg.com>
2019-10-18 13:54:49 -0700

    fix views (#15)

--- providers/snowflake/view.go
+++ providers/snowflake/view.go
@@ -26,12 +26,18 @@ type ViewGenerator struct {
 func (g ViewGenerator) createResources(viewList []view) []terraform_utils.Resource {
 	var resources []terraform_utils.Resource
 	for _, view := range viewList {
-		resources = append(resources, terraform_utils.NewSimpleResource(
+		resources = append(resources, terraform_utils.NewResource(
 			fmt.Sprintf("%s|%s|%s", view.DatabaseName.String, view.SchemaName.String, view.Name.String),
 			fmt.Sprintf("%s__%s__%s", view.DatabaseName.String, view.SchemaName.String, view.Name.String),
 			"snowflake_view",
 			"snowflake",
-			[]string{}))
+			map[string]string{},
+			[]string{},
+			map[string]interface{}{
+				"database":  view.DatabaseName.String,
+				"statement": view.Text.String,
+			},
+		))
 	}
 	return resources
 }
