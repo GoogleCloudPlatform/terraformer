51f8a59ff962028f712265a8152918e4a41b3430
Patryk Orwat <patrykorwat@gmail.com>
2019-11-20 01:25:01 +0700

    #275 switched to simple cycle detection and breaking mechanism; added simple tests

--- providers/aws/sg_test.go
+++ providers/aws/sg_test.go
@@ -15,22 +15,54 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"reflect"
 	"testing"
 )
 
-func TestCycleReference(t *testing.T) {
-	securityGroups := []*ec2.SecurityGroup{
+func TestEmptySgs(t *testing.T) {
+	var securityGroups []*ec2.SecurityGroup
 
+	rulesToMoveOut := findRulesToMoveOut(securityGroups)
+
+	if rulesToMoveOut != nil {
+		t.Errorf("failed to calculate rules to move out %v", rulesToMoveOut)
+	}
+}
+func TestCycleReference(t *testing.T) {
+	sgA := ec2.SecurityGroup{
+		GroupId: aws.String("aaaa"),
+		IpPermissions: []*ec2.IpPermission{
+			{
+				UserIdGroupPairs: []*ec2.UserIdGroupPair{
+					{
+						GroupId: aws.String("bbbb"),
+					},
+				},
+			},
+			{},
+		},
+	}
+	securityGroups := []*ec2.SecurityGroup{
+		&sgA,
+		{
+			GroupId: aws.String("bbbb"),
+			IpPermissions: []*ec2.IpPermission{
+				{
+					UserIdGroupPairs: []*ec2.UserIdGroupPair{
+						{
+							GroupId: aws.String("aaaa"),
+						},
+					},
+				},
+			},
+		},
 	}
 
-	spanningTree := findSpanningTree(securityGroups)
+	rulesToMoveOut := findRulesToMoveOut(securityGroups)
 
-
-	if !reflect.DeepEqual(spanningTree, map[string]interface{}{
-
-	}) {
-		t.Errorf("failed to calculate spanning tree %v", spanningTree)
+	if !reflect.DeepEqual(rulesToMoveOut[0].sourceSG, &sgA) {
+		t.Errorf("failed to calculate rules to move out %v", rulesToMoveOut)
 	}
 }
