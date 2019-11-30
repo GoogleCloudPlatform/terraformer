e147e596051472db5e530dfaf1ff07b2550e2ce4
Austin <cheungaustin2@gmail.com>
2019-11-29 23:08:42 -0600

    add support for vultr dns record

--- providers/vultr/dns_domain.go
+++ providers/vultr/dns_domain.go
@@ -16,6 +16,7 @@ package vultr
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/GoogleCloudPlatform/terraformer/terraform_utils"
 	"github.com/vultr/govultr"
@@ -25,25 +26,51 @@ type DNSDomainGenerator struct {
 	VultrService
 }
 
-func (g DNSDomainGenerator) createResources(domainList []govultr.DNSDomain) []terraform_utils.Resource {
-	var resources []terraform_utils.Resource
+func (g *DNSDomainGenerator) loadDNSDomains(client *govultr.Client) ([]govultr.DNSDomain, error) {
+	domainList, err := client.DNSDomain.List(context.Background())
+	if err != nil {
+		return nil, err
+	}
 	for _, domain := range domainList {
-		resources = append(resources, terraform_utils.NewSimpleResource(
+		g.Resources = append(g.Resources, terraform_utils.NewSimpleResource(
 			domain.Domain,
 			domain.Domain,
 			"vultr_dns_domain",
 			"vultr",
 			[]string{}))
 	}
-	return resources
+	return domainList, nil
+}
+
+func (g *DNSDomainGenerator) loadDNSRecords(client *govultr.Client, domain string) error {
+	recordList, err := client.DNSRecord.List(context.Background(), domain)
+	if err != nil {
+		return err
+	}
+	for _, record := range recordList {
+		g.Resources = append(g.Resources, terraform_utils.NewResource(
+			strconv.Itoa(record.RecordID),
+			strconv.Itoa(record.RecordID),
+			"vultr_dns_record",
+			"vultr",
+			map[string]string{"domain": domain},
+			[]string{},
+			map[string]interface{}{}))
+	}
+	return nil
 }
 
 func (g *DNSDomainGenerator) InitResources() error {
 	client := g.generateClient()
-	output, err := client.DNSDomain.List(context.Background())
+	domainList, err := g.loadDNSDomains(client)
 	if err != nil {
 		return err
 	}
-	g.Resources = g.createResources(output)
+	for _, domain := range domainList {
+		err := g.loadDNSRecords(client, domain.Domain)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
