b2f92118ffed2fb6c91efee1b04034f439b3adcd
Austin Cheung <cheungaustin2@gmail.com>
2020-02-29 18:42:22 -0800

    add support for aws_devicefarm_project

---
+++ providers/aws/devicefarm.go
@@ -0,0 +1,53 @@
+// Copyright 2020 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"context"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraform_utils"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/devicefarm"
+)
+
+var devicefarmAllowEmptyValues = []string{"tags."}
+
+type DeviceFarmGenerator struct {
+	AWSService
+}
+
+func (g *DeviceFarmGenerator) InitResources() error {
+	config, e := g.generateConfig()
+	if e != nil {
+		return e
+	}
+	svc := devicefarm.New(config)
+	p := devicefarm.NewListProjectsPaginator(svc.ListProjectsRequest(&devicefarm.ListProjectsInput{}))
+	var resources []terraform_utils.Resource
+	for p.Next(context.Background()) {
+		for _, project := range p.CurrentPage().Projects {
+			projectArn := aws.StringValue(project.Arn)
+			projectName := aws.StringValue(project.Name)
+			resources = append(resources, terraform_utils.NewSimpleResource(
+				projectArn,
+				projectName,
+				"aws_devicefarm_project",
+				"aws",
+				devicefarmAllowEmptyValues))
+		}
+	}
+	g.Resources = resources
+	return p.Err()
+}
