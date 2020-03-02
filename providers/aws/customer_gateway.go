0a0ce07989e3473c6924d10cb37932577af6bd25
David Barranco <dbarranco@vmware.com>
2020-03-02 14:33:33 +0100

    fix VMware copyright notice
    Signed-off-by: David Barranco <dbarranco@vmware.com>

--- providers/aws/customer_gateway.go
+++ providers/aws/customer_gateway.go
@@ -1,5 +1,4 @@
-// Copyright (c) 2020 VMware, Inc.
-// SPDX-License-Identifier: Apache-2.0
+// Copyright 2018 The Terraformer Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
