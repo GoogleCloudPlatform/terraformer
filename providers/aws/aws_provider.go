256ea8ca410c97b0b54f7ccebcf8deb0f97a0b55
Juno Yu <juno.yuchunlung@gmail.com>
2020-03-30 19:35:10 +0800

    Add xray support

--- providers/aws/aws_provider.go
+++ providers/aws/aws_provider.go
@@ -292,5 +292,6 @@ func (p *AWSProvider) GetSupportedService() map[string]terraform_utils.ServiceGe
 		"vpc_peering":       &VpcPeeringConnectionGenerator{},
 		"vpn_connection":    &VpnConnectionGenerator{},
 		"vpn_gateway":       &VpnGatewayGenerator{},
+		"xray":              &XrayGenerator{},
 	}
 }
