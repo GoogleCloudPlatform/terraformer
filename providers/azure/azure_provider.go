965952a145395cb9232b1578e45ff37b4e072d2b
rotemavni <rotemavn@gmail.com>
2020-07-06 15:23:40 +0300

    added support to Azure app service

--- providers/azure/azure_provider.go
+++ providers/azure/azure_provider.go
@@ -145,6 +145,7 @@ func (AzureProvider) GetResourceConnections() map[string]map[string][]string {
 
 func (p *AzureProvider) GetSupportedService() map[string]terraformutils.ServiceGenerator {
 	return map[string]terraformutils.ServiceGenerator{
+		"app_service":            &AppServiceGenerator{},
 		"analysis":               &AnalysisGenerator{},
 		"cosmosdb":               &CosmosDBGenerator{},
 		"database":               &DatabasesGenerator{},
