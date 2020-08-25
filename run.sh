0338543c68293a679561d198cb49a038fcbf033a
Or Shapira <orshapira91@gmail.com>
2020-08-25 10:42:36 +0300

    print

--- run.sh
+++ run.sh
@@ -41,6 +41,7 @@ run_terraformer(){
 			else
 				regions="us-east-1,us-east-2,us-west-1,us-west-2,af-south-1,ap-east-1,ap-south-1,ap-southeast-1,ap-southeast-2,ap-northeast-1,ap-northeast-2,ca-central-1,eu-central-1,eu-west-1,eu-west-2,eu-west-3,eu-south-1,eu-north-1,me-south-1,se-east-1"
 			fi
+			echo "!!!!import aws -r ${1} -z ${regions}"
 			./terraformer-aws import aws -r ${1} -z ${regions}
 			aws s3 sync --delete ${path}/${1}/ s3://${RESULT_BUCKET}/terraformer/${CUSTOMER_NAME}/${ACCOUNT_ID}/${TIMESTAMP}/${1}/
 			;;
