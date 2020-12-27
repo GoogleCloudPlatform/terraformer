f89c5320d6227c2f6386843d656c5a179c772a74
Luis San Martin <luis@sanmartin.io>
2020-12-27 21:57:29 +0100

    clean gitignore && remove Err

--- terraformutils/providerwrapper/provider.go
+++ terraformutils/providerwrapper/provider.go
@@ -157,7 +157,6 @@ func (p *ProviderWrapper) Refresh(info *terraform.InstanceInfo, state *terraform
 		})
 		if resp.Diagnostics.HasErrors() {
 			log.Println("WARN: Fail read resource from provider, wait 300ms before retry")
-			log.Println(resp.Diagnostics.Err())
 			time.Sleep(300 * time.Millisecond)
 			continue
 		} else {
