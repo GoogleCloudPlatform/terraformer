7b897d028de0544def6d74f2d38c499951d5d955
rotemavni <rotemavn@gmail.com>
2021-01-21 11:06:11 +0200

    reduce number of retries and increase sleep period

--- run.sh
+++ run.sh
@@ -44,7 +44,7 @@ run_terraformer(){
 			elif [[ $1 == "eks" ]]; then
   			./terraformer-aws import aws --profile ${ACCOUNT_ID} --resources ${1} --regions us-east-1,us-east-2,us-west-2,ap-south-1,ap-southeast-1,ap-southeast-2,ap-northeast-1,ap-northeast-2,ca-central-1,eu-central-1,eu-west-1,eu-west-2,eu-west-3,eu-north-1,sa-east-1 -c=false || true
 			else
-  			./terraformer-aws import aws --profile ${ACCOUNT_ID} --resources ${1} --regions us-east-1,us-east-2,us-west-1,us-west-2,ca-central-1,sa-east-1 -c=false || true
+  			./terraformer-aws import aws --profile ${ACCOUNT_ID} --resources ${1} --regions us-east-1,us-east-2,us-west-1,us-west-2,ca-central-1,sa-east-1 -c=false --retry-number=2 --retry-sleep-ms=5000 -|| true
   			./terraformer-aws import aws --profile ${ACCOUNT_ID} --resources ${1} --regions ap-south-1,ap-southeast-1,ap-southeast-2,ap-northeast-1,ap-northeast-2 -c=false || true
   			./terraformer-aws import aws --profile ${ACCOUNT_ID} --resources ${1} --regions eu-central-1,eu-west-1,eu-west-2,eu-west-3,eu-north-1 -c=false || true
 			fi
