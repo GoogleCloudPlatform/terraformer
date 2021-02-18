247540175d36b8ff190c030858201ad5d669b1f5
rotemavni <rotemavn@gmail.com>
2021-02-18 11:55:41 +0200

    set cloudsql as google global resource

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GLOBAL_GCP_SERVICES=",dns,gcs,globalAddresses,globalForwardingRules,iam,gke,backendServices,bigQuery,disks,firewall,healthChecks,httpHealthChecks,instanceTemplates,networks,project,routes,targetHttpsProxies,urlMaps,"
+GLOBAL_GCP_SERVICES=",dns,gcs,globalAddresses,globalForwardingRules,iam,gke,backendServices,bigQuery,disks,firewall,healthChecks,httpHealthChecks,instanceTemplates,networks,project,routes,targetHttpsProxies,urlMaps,cloudsql,"
 GLOBAL_AWS_SERVICES=",sts,iam,cloudfront,accessanalyzer,"
 
 case $CSP in
