a603fc5566d9036f42f1c04869432755a26bd94d
Joseph D. Marhee <jmarhee@interiorae.com>
2021-04-03 02:57:14 -0500

    Sets up command for Equinix Metal provider

--- cmd/root.go
+++ cmd/root.go
@@ -46,6 +46,7 @@ func providerImporterSubcommands() []func(options ImportOptions) *cobra.Command 
 		newCmdIbmImporter,
 		// Cloud
 		newCmdDigitalOceanImporter,
+		newCmdEquinixMetalImporter,
 		newCmdFastlyImporter,
 		newCmdHerokuImporter,
 		newCmdLinodeImporter,
@@ -85,6 +86,7 @@ func providerGenerators() map[string]func() terraformutils.ProviderGenerator {
 		newIbmProvider,
 		// Cloud
 		newDigitalOceanProvider,
+		newEquinixMetalProvider,
 		newFastlyProvider,
 		newHerokuProvider,
 		newLinodeProvider,
