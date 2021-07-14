58dad324d63406118c2c3c2756de99918cef906b
Sherzod K <sherzod.karimov@datadoghq.com>
2021-07-14 15:58:31 -0400

    fix missing widgets

--- terraformutils/providerwrapper/provider.go
+++ terraformutils/providerwrapper/provider.go
@@ -151,7 +151,8 @@ func (p *ProviderWrapper) readObjBlocks(block map[string]*configschema.NestedBlo
 				}
 			}
 		}
-		if fieldCount == len(v.Block.Attributes) && fieldCount > 0 {
+		log.Println("check this", len(v.BlockTypes))
+		if fieldCount == len(v.Block.Attributes) && fieldCount > 0 && len(v.BlockTypes) == 0 {
 			readOnlyAttributes = append(readOnlyAttributes, "^"+k)
 		}
 	}
