c49ef2d5a358d16bfda287018bef83edac30d0d6
Rachel Aurand <rachel@countableset.com>
2022-03-11 10:17:29 -0800

    add logging config and associatations

--- providers/aws/wafv2.go
+++ providers/aws/wafv2.go
@@ -23,6 +23,7 @@ import (
 )
 
 var wafv2AllowEmptyValues = []string{"tags."}
+var scope = types.ScopeRegional
 
 type Wafv2Generator struct {
 	AWSService
@@ -47,13 +48,15 @@ func (g *Wafv2Generator) InitResources() error {
 	if err := g.loadWafRuleGroups(svc); err != nil {
 		return err
 	}
+	if err := g.loadWebACLLoggingConfiguration(svc); err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func (g *Wafv2Generator) loadWebACL(svc *wafv2.Client) error {
-	//svc.GetWebACL(context.TODO(), &wafv2.GetWebACLInput{})
-	output, err := svc.ListWebACLs(context.TODO(), &wafv2.ListWebACLsInput{Scope: types.ScopeCloudfront})
+	output, err := svc.ListWebACLs(context.TODO(), &wafv2.ListWebACLsInput{Scope: scope})
 	if err != nil {
 		return err
 	}
@@ -65,7 +68,33 @@ func (g *Wafv2Generator) loadWebACL(svc *wafv2.Client) error {
 			"aws",
 			map[string]string{
 				"name":  *acl.Name,
-				"scope": string(types.ScopeCloudfront),
+				"scope": string(scope),
+			},
+			wafv2AllowEmptyValues,
+			map[string]interface{}{},
+		))
+		err = g.loadWebACLAssociations(svc, acl.ARN)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (g *Wafv2Generator) loadWebACLAssociations(svc *wafv2.Client, webACLArn *string) error {
+	output, err := svc.ListResourcesForWebACL(context.TODO(), &wafv2.ListResourcesForWebACLInput{WebACLArn: webACLArn})
+	if err != nil {
+		return err
+	}
+	for _, resource := range output.ResourceArns {
+		g.Resources = append(g.Resources, terraformutils.NewResource(
+			resource,
+			resource,
+			"aws_wafv2_web_acl_association",
+			"aws",
+			map[string]string{
+				"resource_arn": resource,
+				"web_acl_arn":  *webACLArn,
 			},
 			wafv2AllowEmptyValues,
 			map[string]interface{}{},
@@ -75,7 +104,7 @@ func (g *Wafv2Generator) loadWebACL(svc *wafv2.Client) error {
 }
 
 func (g *Wafv2Generator) loadIPSet(svc *wafv2.Client) error {
-	output, err := svc.ListIPSets(context.TODO(), &wafv2.ListIPSetsInput{Scope: types.ScopeCloudfront})
+	output, err := svc.ListIPSets(context.TODO(), &wafv2.ListIPSetsInput{Scope: scope})
 	if err != nil {
 		return err
 	}
@@ -97,8 +126,7 @@ func (g *Wafv2Generator) loadIPSet(svc *wafv2.Client) error {
 }
 
 func (g *Wafv2Generator) loadRegexPatternSets(svc *wafv2.Client) error {
-	//svc.GetRegexPatternSet(context.TODO(), &wafv2.GetRegexPatternSetInput{})
-	output, err := svc.ListRegexPatternSets(context.TODO(), &wafv2.ListRegexPatternSetsInput{Scope: types.ScopeCloudfront})
+	output, err := svc.ListRegexPatternSets(context.TODO(), &wafv2.ListRegexPatternSetsInput{Scope: scope})
 	if err != nil {
 		return err
 	}
@@ -120,7 +148,7 @@ func (g *Wafv2Generator) loadRegexPatternSets(svc *wafv2.Client) error {
 }
 
 func (g *Wafv2Generator) loadWafRuleGroups(svc *wafv2.Client) error {
-	output, err := svc.ListRuleGroups(context.TODO(), &wafv2.ListRuleGroupsInput{Scope: types.ScopeCloudfront})
+	output, err := svc.ListRuleGroups(context.TODO(), &wafv2.ListRuleGroupsInput{Scope: scope})
 	if err != nil {
 		return err
 	}
@@ -134,6 +162,27 @@ func (g *Wafv2Generator) loadWafRuleGroups(svc *wafv2.Client) error {
 				"arn":   *ruleGroup.ARN,
 				"name":  *ruleGroup.Name,
 				"scope": string(types.ScopeCloudfront),
+			},
+			wafv2AllowEmptyValues,
+			map[string]interface{}{},
+		))
+	}
+	return nil
+}
+
+func (g *Wafv2Generator) loadWebACLLoggingConfiguration(svc *wafv2.Client) error {
+	output, err := svc.ListLoggingConfigurations(context.TODO(), &wafv2.ListLoggingConfigurationsInput{Scope: scope})
+	if err != nil {
+		return err
+	}
+	for _, logConfig := range output.LoggingConfigurations {
+		g.Resources = append(g.Resources, terraformutils.NewResource(
+			*logConfig.ResourceArn,
+			*logConfig.ResourceArn,
+			"aws_wafv2_web_acl_logging_configuration",
+			"aws",
+			map[string]string{
+				"resource_arn": *logConfig.ResourceArn,
 			},
 			wafv2AllowEmptyValues,
 			map[string]interface{}{},
