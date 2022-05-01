b2678847f15b42ecc353fed516b5befd57b258c8
Sergey Lanzman <sergeylanz@gmail.com>
2022-05-01 21:46:58 +0300

    improve auth0 provider

---
+++ providers/auth0/tenant.go
@@ -0,0 +1,53 @@
+// Copyright 2022 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth0
+
+import (
+	"encoding/base64"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
+	"gopkg.in/auth0.v5/management"
+)
+
+var (
+	TenantAllowEmptyValues = []string{}
+)
+
+type TenantGenerator struct {
+	Auth0Service
+}
+
+func (g TenantGenerator) createResources(tenant *management.Tenant) []terraformutils.Resource {
+	resources := []terraformutils.Resource{}
+	resourceName := base64.StdEncoding.EncodeToString([]byte(tenant.String()))
+	resources = append(resources, terraformutils.NewSimpleResource(
+		resourceName,
+		resourceName,
+		"auth0_tenant",
+		"auth0",
+		TenantAllowEmptyValues,
+	))
+	return resources
+}
+
+func (g *TenantGenerator) InitResources() error {
+	m := g.generateClient()
+	Tenant, err := m.Tenant.Read()
+	if err != nil {
+		return err
+	}
+	g.Resources = g.createResources(Tenant)
+	return nil
+}
