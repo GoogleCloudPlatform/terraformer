bbf44edaaa9999281f16623bb8e3f2c67ae67f75
Sergey Lanzman <sergeylanz@gmail.com>
2022-06-01 13:02:26 +0300

    add launchdarkly_feature_flag_environment (#1336)

--- providers/launchdarkly/feature_flags.go
+++ providers/launchdarkly/feature_flags.go
@@ -27,6 +27,28 @@ type FeatureFlagsGenerator struct {
 	LaunchDarklyService
 }
 
+func (g *FeatureFlagsGenerator) loadFeatureFlagEnv(ctx context.Context, client *launchdarkly.APIClient, projectKey, flagKey string) error {
+	ff, _, err := client.FeatureFlagsApi.GetFeatureFlag(ctx, projectKey, flagKey, &launchdarkly.FeatureFlagsApiGetFeatureFlagOpts{})
+	if err != nil {
+		return err
+	}
+	for envKey := range ff.Environments {
+		resource := terraformutils.NewResource(
+			projectKey+"/"+envKey+"/"+flagKey,
+			projectKey+"-"+envKey+"-"+flagKey,
+			"launchdarkly_feature_flag_environment",
+			"launchdarkly",
+			map[string]string{
+				"env_key": envKey,
+				"flag_id": projectKey + "/" + flagKey,
+			},
+			featureFlagsAllowEmptyValues,
+			map[string]interface{}{})
+		g.Resources = append(g.Resources, resource)
+	}
+	return nil
+}
+
 func (g *FeatureFlagsGenerator) loadFeatureFlags(ctx context.Context, client *launchdarkly.APIClient, project string) error {
 	featureFlags, _, err := client.FeatureFlagsApi.GetFeatureFlags(ctx, project, &launchdarkly.FeatureFlagsApiGetFeatureFlagsOpts{})
 	if err != nil {
@@ -45,6 +67,10 @@ func (g *FeatureFlagsGenerator) loadFeatureFlags(ctx context.Context, client *la
 			featureFlagsAllowEmptyValues,
 			map[string]interface{}{})
 		resource.IgnoreKeys = append(resource.IgnoreKeys, "include_in_snippet")
+		err = g.loadFeatureFlagEnv(ctx, client, project, featureFlag.Key)
+		if err != nil {
+			return err
+		}
 		g.Resources = append(g.Resources, resource)
 	}
 	return nil
