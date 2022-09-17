2bd7435aacca0acc3aa017272f806edc4c53f6b3
Vishal Sahu <vishal@sumologic.com>
2022-09-16 23:33:03 -0700

    add dashboard resource

--- providers/sumologic/sumologic_provider.go
+++ providers/sumologic/sumologic_provider.go
@@ -75,6 +75,7 @@ func (p *SumoLogicProvider) GetConfig() cty.Value {
 
 func (p *SumoLogicProvider) GetSupportedService() map[string]terraformutils.ServiceGenerator {
 	return map[string]terraformutils.ServiceGenerator{
+		"dashboard":             &DashboardGenerator{},
 		"field_extraction_rule": &FieldExtractionRuleGenerator{},
 		"monitor":               &MonitorGenerator{},
 		"partition":             &PartitionGenerator{},
