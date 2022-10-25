3a1e8406de69f6df300b32ab3fa2646611f4bde0
adya1303 <adya.nand.jha1@ibm.com>
2022-10-26 04:13:27 +0530

    Add resource conn (#1504)
    * Added ibm_is_instance_volume_attachment
    
    * removed extra information causing conflicts
    
    * added ibm_is_placement_group
    
    * moved placement to diff file
    
    * Added ibm docs changes
    
    * Revert "added ibm_is_placement_group"
    
    * added resource connection

--- providers/ibm/ibm_is_instance.go
+++ providers/ibm/ibm_is_instance.go
@@ -52,6 +52,24 @@ func (g InstanceGenerator) createInstanceResources(instanceID, instanceName, ins
 	return resource
 }
 
+func (g InstanceGenerator) createVPCVolumeAttachmentResource(instanceID, volumeAttachedID, volumeAttachedName string) terraformutils.Resource {
+	resource := terraformutils.NewResource(
+		fmt.Sprintf("%s/%s", instanceID, volumeAttachedID),
+		normalizeResourceName(volumeAttachedName, true),
+		"ibm_is_instance_volume_attachment",
+		"ibm",
+		map[string]string{},
+		[]string{},
+		map[string]interface{}{})
+
+	resource.IgnoreKeys = append(resource.IgnoreKeys,
+		"^volume$",
+		"^iops$",
+	)
+
+	return resource
+}
+
 // InitResources ...
 func (g *InstanceGenerator) InitResources() error {
 	region := g.Args["region"].(string)
@@ -100,6 +118,38 @@ func (g *InstanceGenerator) InitResources() error {
 
 	for _, instance := range allrecs {
 		g.Resources = append(g.Resources, g.createInstanceResources(*instance.ID, *instance.Name, *instance.Image.ID))
+		listVPCInsVolOptions := &vpcv1.ListInstanceVolumeAttachmentsOptions{
+			InstanceID: instance.ID,
+		}
+
+		volumeAtts, response, err := vpcclient.ListInstanceVolumeAttachments(listVPCInsVolOptions)
+		if err != nil {
+			return fmt.Errorf("fetching vpc Instance volume Attachments %s\n%s", err, response)
+		}
+		allrecs := []vpcv1.VolumeAttachment{}
+		allrecs = append(allrecs, volumeAtts.VolumeAttachments...)
+
+		for _, volumeAtt := range allrecs {
+			g.Resources = append(g.Resources, g.createVPCVolumeAttachmentResource(*instance.ID, *volumeAtt.ID, *volumeAtt.Name))
+		}
 	}
+	return nil
+}
+
+func (g *InstanceGenerator) PostConvertHook() error {
+	for i, r := range g.Resources {
+		if r.InstanceInfo.Type != "ibm_is_instance_volume_attachment" {
+			continue
+		}
+		for _, ri := range g.Resources {
+			if ri.InstanceInfo.Type != "ibm_is_instance" {
+				continue
+			}
+			if r.InstanceState.Attributes["instance"] == ri.InstanceState.Attributes["id"] {
+				g.Resources[i].Item["instance"] = "${ibm_is_instance." + ri.ResourceName + ".id}"
+			}
+		}
+	}
+
 	return nil
 }
