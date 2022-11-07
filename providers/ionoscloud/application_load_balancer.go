4c8621f21ee71e763ece86a2dee5f029d98a952c
Deatcu Andrei-Cristian <113900140+adeatcu-ionos@users.noreply.github.com>
2022-11-07 07:41:44 +0200

    feat: implement application load balancer (#15)

---
+++ providers/ionoscloud/application_load_balancer.go
@@ -0,0 +1,54 @@
+package ionoscloud
+
+import (
+	"context"
+	"github.com/GoogleCloudPlatform/terraformer/providers/ionoscloud/helpers"
+	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
+	"log"
+)
+
+type ApplicationLoadBalancer struct {
+	IonosCloudService
+}
+
+func (g *ApplicationLoadBalancer) InitResources() error {
+	client := g.generateClient()
+	cloudApiClient := client.CloudApiClient
+	resource_type := "ionoscloud_application_loadbalancer"
+	datacenters, err := helpers.GetAllDatacenters(*cloudApiClient)
+	if err != nil {
+		return err
+	}
+	for _, datacenter := range datacenters {
+		applicationLoadBalancerResponse, _, err := cloudApiClient.ApplicationLoadBalancersApi.DatacentersApplicationloadbalancersGet(context.TODO(), *datacenter.Id).Depth(1).Execute()
+		if err != nil {
+			return err
+		}
+		if applicationLoadBalancerResponse.Items == nil {
+			log.Printf(
+				"[WARNING] expected a response containing application load balancers but received 'nil' instead, skipping search for datacenter with ID: %v.\n",
+				*datacenter.Id)
+			continue
+		}
+		applicationLoadBalancers := *applicationLoadBalancerResponse.Items
+		for _, applicationLoadBalancer := range applicationLoadBalancers {
+			if applicationLoadBalancer.Properties == nil || applicationLoadBalancer.Properties.Name == nil {
+				log.Printf(
+					"[WARNING] 'nil' values in the response for application load balancer with ID %v, datacenter ID: %v, skipping this resource.\n",
+					*applicationLoadBalancer.Id,
+					*datacenter.Id,
+				)
+				continue
+			}
+			g.Resources = append(g.Resources, terraformutils.NewResource(
+				*applicationLoadBalancer.Id,
+				*applicationLoadBalancer.Properties.Name+"-"+*applicationLoadBalancer.Id,
+				resource_type,
+				helpers.Ionos,
+				map[string]string{helpers.DcId: *datacenter.Id},
+				[]string{},
+				map[string]interface{}{}))
+		}
+	}
+	return nil
+}
