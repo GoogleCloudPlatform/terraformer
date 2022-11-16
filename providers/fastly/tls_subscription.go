9ac97d67d2f71601e0e7baf2e3961097041c0bfb
Rui Chen <rui@chenrui.dev>
2022-11-16 10:51:44 -0500

    deps(fastly): bump to use go-fastly v7
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- providers/fastly/tls_subscription.go
+++ providers/fastly/tls_subscription.go
@@ -18,7 +18,7 @@ import (
 	"log"
 
 	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
-	"github.com/fastly/go-fastly/v6/fastly"
+	"github.com/fastly/go-fastly/v7/fastly"
 )
 
 type TLSSubscriptionGenerator struct {
