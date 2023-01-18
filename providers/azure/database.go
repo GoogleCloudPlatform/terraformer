34149f412c65afdb62c7130e40b2ed39ba40c1e4
Andrey Andreev <duke66@yandex.ru>
2023-01-18 14:40:46 +0100

    fix: remove the infinite loop on azure psql servers virtual network rules and sql failover

--- providers/azure/database.go
+++ providers/azure/database.go
@@ -546,6 +546,10 @@ func (g *DatabasesGenerator) createPostgreSQLVirtualNetworkRuleResources(servers
 				"azurerm_postgresql_virtual_network_rule",
 				g.ProviderName,
 				[]string{}))
+
+			if err := rulePages.NextWithContext(ctx); err != nil {
+				return nil, err
+			}
 		}
 	}
 	return resources, nil
@@ -760,6 +764,10 @@ func (g *DatabasesGenerator) createSQLFailoverResources(servers []sql.Server) ([
 				"azurerm_sql_failover_group",
 				g.ProviderName,
 				[]string{}))
+
+			if err := iter.NextWithContext(ctx); err != nil {
+				return nil, err
+			}
 		}
 	}
 	return resources, nil
