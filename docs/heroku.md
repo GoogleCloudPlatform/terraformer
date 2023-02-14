117286d8e903d5ddee9f4e9bd2a7268b3b2ec823
Mars Hall <mars@heroku.com>
2023-02-13 17:14:22 -0800

    heroku_addon_attachments filtered by owning app

--- docs/heroku.md
+++ docs/heroku.md
@@ -16,7 +16,7 @@ export HEROKU_API_KEY=<token>
 ./terraformer import heroku --resources=app --team=<name>
 ./terraformer import heroku --resources=app --filter=app=<ID>
 
-./terraformer import heroku --resources=app,addon --filter=app=<ID>
+./terraformer import heroku --resources=app,addon,addon_attachment --filter=app=<ID>
 
 ./terraformer import heroku --resources=account_feature
 ```
--- providers/heroku/addon_attachment.go
+++ providers/heroku/addon_attachment.go
@@ -16,6 +16,7 @@ package heroku
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
 	heroku "github.com/heroku/heroku-go/v5"
@@ -30,7 +31,7 @@ func (g AddOnAttachmentGenerator) createResources(addOnAttachmentList []heroku.A
 	for _, addOnAttachment := range addOnAttachmentList {
 		resources = append(resources, terraformutils.NewSimpleResource(
 			addOnAttachment.ID,
-			addOnAttachment.Name,
+			fmt.Sprintf("%s-%s", addOnAttachment.App.Name, addOnAttachment.Name),
 			"heroku_addon_attachment",
 			"heroku",
 			[]string{}))
@@ -40,10 +41,37 @@ func (g AddOnAttachmentGenerator) createResources(addOnAttachmentList []heroku.A
 
 func (g *AddOnAttachmentGenerator) InitResources() error {
 	svc := g.generateService()
-	output, err := svc.AddOnAttachmentList(context.TODO(), &heroku.ListRange{Field: "id"})
-	if err != nil {
-		return err
+	ctx := context.Background()
+
+	var output []heroku.AddOnAttachment
+	var hasRequiredFilter bool
+
+	if len(g.Filter) > 0 {
+		for _, filter := range g.Filter {
+			if filter.IsApplicable("app") {
+				hasRequiredFilter = true
+				for _, appID := range filter.AcceptableValues {
+					appAddons, err := svc.AddOnListByApp(ctx, appID, &heroku.ListRange{Field: "id", Max: 1000})
+					if err != nil {
+						return fmt.Errorf("Error filtering addons by app '%s': %w", appID, err)
+					}
+					for _, addOn := range appAddons {
+						addonAttachments, err := svc.AddOnAttachmentListByAddOn(ctx, addOn.ID, &heroku.ListRange{Field: "id", Max: 1000})
+						if err != nil {
+							return fmt.Errorf("Error filtering addon attachments by app '%s': %w", appID, err)
+						}
+						for _, attachment := range addonAttachments {
+							output = append(output, attachment)
+						}
+					}
+				}
+			}
+		}
 	}
+	if !hasRequiredFilter {
+		return fmt.Errorf("Heroku Addons Attachments must be filtered by app: --filter=app=<ID>")
+	}
+
 	g.Resources = g.createResources(output)
 	return nil
 }
