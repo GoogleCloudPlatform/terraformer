086fed9c2d8e4528551ec19eae90d1f8cf676860
Bojan Ristic <e.tachka@gmail.com>
2023-07-18 17:42:24 +0200

    iam

--- providers/gcp/apiGateway.go
+++ providers/gcp/apiGateway.go
@@ -51,19 +51,23 @@ func (g *ApiGatewayGenerator) createApis(client *apigateway.Client, it *apigatew
 		project := g.GetArgs()["project"].(string)
 		location := g.GetArgs()["region"].(compute.Region).Name
 
-		g.Resources = append(g.Resources, terraformutils.NewResource(
-			api.GetName(),
-			api.GetDisplayName(),
-			"google_api_gateway_api",
-			g.GetProviderName(),
-			map[string]string{
-				"name":    api.GetName(),
-				"project": project,
-				"region":  location,
-			},
-			apiGatewaysAllowEmptyValues,
-			apiGatewaysAdditionalFields,
-		))
+		labels := []string{"api", "api_iam_policy", "api_iam_binding", "api_iam_member"}
+
+		for _, label := range labels {
+			g.Resources = append(g.Resources, terraformutils.NewResource(
+				api.GetName(),
+				api.GetDisplayName(),
+				fmt.Sprintf("google_api_gateway_%s", label),
+				g.GetProviderName(),
+				map[string]string{
+					"name":    api.GetName(),
+					"project": project,
+					"region":  location,
+				},
+				apiGatewaysAllowEmptyValues,
+				apiGatewaysAdditionalFields,
+			))
+		}
 
 		configsIterator := client.ListApiConfigs(context.Background(), &pb.ListApiConfigsRequest{Parent: fmt.Sprintf("projects/%s/locations/global/apis/%s", project, api.GetDisplayName())})
 		if err := g.createConfigs(configsIterator); err != nil {
@@ -84,19 +88,23 @@ func (g *ApiGatewayGenerator) createConfigs(it *apigateway.ApiConfigIterator) er
 			return err
 		}
 
-		g.Resources = append(g.Resources, terraformutils.NewResource(
-			obj.GetName(),
-			obj.GetDisplayName(),
-			"google_api_gateway_api_config",
-			g.GetProviderName(),
-			map[string]string{
-				"name":    obj.GetName(),
-				"project": g.GetArgs()["project"].(string),
-				"region":  g.GetArgs()["region"].(compute.Region).Name,
-			},
-			apiGatewaysAllowEmptyValues,
-			apiGatewaysAdditionalFields,
-		))
+		labels := []string{"config", "config_iam_policy", "config_iam_binding", "config_iam_member"}
+
+		for _, label := range labels {
+			g.Resources = append(g.Resources, terraformutils.NewResource(
+				obj.GetName(),
+				obj.GetDisplayName(),
+				fmt.Sprintf("google_api_gateway_api_%s", label),
+				g.GetProviderName(),
+				map[string]string{
+					"name":    obj.GetName(),
+					"project": g.GetArgs()["project"].(string),
+					"region":  g.GetArgs()["region"].(compute.Region).Name,
+				},
+				apiGatewaysAllowEmptyValues,
+				apiGatewaysAdditionalFields,
+			))
+		}
 	}
 }
 
@@ -107,18 +115,22 @@ func (g *ApiGatewayGenerator) createGateways(it *apigateway.GatewayIterator) err
 			return err
 		}
 
-		g.Resources = append(g.Resources, terraformutils.NewResource(
-			obj.GetName(),
-			obj.GetDisplayName(),
-			"google_api_gateway_gateway",
-			g.GetProviderName(),
-			map[string]string{
-				"name":    obj.GetName(),
-				"project": g.GetArgs()["project"].(string),
-				"region":  g.GetArgs()["region"].(compute.Region).Name,
-			},
-			apiGatewaysAllowEmptyValues,
-			apiGatewaysAdditionalFields,
-		))
+		labels := []string{"gateway", "gateway_iam_policy", "gateway_iam_binding", "gateway_iam_member"}
+
+		for _, label := range labels {
+			g.Resources = append(g.Resources, terraformutils.NewResource(
+				obj.GetName(),
+				obj.GetDisplayName(),
+				fmt.Sprintf("google_api_gateway_%s", label),
+				g.GetProviderName(),
+				map[string]string{
+					"name":    obj.GetName(),
+					"project": g.GetArgs()["project"].(string),
+					"region":  g.GetArgs()["region"].(compute.Region).Name,
+				},
+				apiGatewaysAllowEmptyValues,
+				apiGatewaysAdditionalFields,
+			))
+		}
 	}
 }
