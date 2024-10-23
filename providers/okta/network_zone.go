88b9fc33ae546eb9cecffeb95a5282e9d3e2e71b
Brenno Oliveira <brenno.oliveira@deliveryhero.com>
2024-10-23 12:57:42 +0200

    update network_zone generator
    Signed-off-by: Brenno Oliveira <brenno.oliveira@deliveryhero.com>

--- providers/okta/network_zone.go
+++ providers/okta/network_zone.go
@@ -15,74 +15,78 @@
 package okta
 
 import (
+	"fmt"
+
 	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
-	"github.com/okta/okta-sdk-golang/v2/okta"
+	"github.com/okta/okta-sdk-golang/v5/okta"
 )
 
 type NetworkZoneGenerator struct {
 	OktaService
 }
 
-func (g NetworkZoneGenerator) createResources(networkZoneList []*okta.NetworkZone) []terraformutils.Resource {
+func (g *NetworkZoneGenerator) createResources(networkZoneList []okta.ListNetworkZones200ResponseInner) []terraformutils.Resource {
 	var resources []terraformutils.Resource
-	for _, networkZone := range networkZoneList {
 
-		resources = append(resources, terraformutils.NewResource(
-			networkZone.Id,
-			networkZone.Name,
-			"okta_network_zone",
-			"okta",
-			map[string]string{
-				"name": networkZone.Name,
-				"type": networkZone.Type,
-			},
-			[]string{},
-			attributesNetworkZone(networkZone),
-		))
+	for _, networkZone := range networkZoneList {
+		var id, name, zoneType *string
+
+		// Handle each type of network zone
+		switch {
+		case networkZone.DynamicNetworkZone != nil:
+			id = networkZone.DynamicNetworkZone.Id
+			name = &networkZone.DynamicNetworkZone.Name
+			zoneType = &networkZone.DynamicNetworkZone.Type
+		case networkZone.EnhancedDynamicNetworkZone != nil:
+			id = networkZone.EnhancedDynamicNetworkZone.Id
+			name = &networkZone.EnhancedDynamicNetworkZone.Name
+			zoneType = &networkZone.EnhancedDynamicNetworkZone.Type
+		case networkZone.IPNetworkZone != nil:
+			id = networkZone.IPNetworkZone.Id
+			name = &networkZone.IPNetworkZone.Name
+			zoneType = &networkZone.IPNetworkZone.Type
+		default:
+			fmt.Println("Unknown or unsupported network zone type encountered")
+			continue
+		}
+
+		// Ensure all required fields are present before creating the resource
+		if id != nil && *name != "" && *zoneType != "" {
+			resource := terraformutils.NewSimpleResource(
+				*id,
+				normalizeResourceName(*id+"_"+*name),
+				"okta_network_zone",
+				"okta",
+				[]string{},
+			)
+			resources = append(resources, resource)
+		}
 	}
 	return resources
 }
 
 func (g *NetworkZoneGenerator) InitResources() error {
-	ctx, client, err := g.Client()
+	ctx, client, err := g.ClientV5()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create Okta client: %w", err)
 	}
 
-	output, resp, err := client.NetworkZone.ListNetworkZones(ctx, nil)
+	networkZoneList, resp, err := client.NetworkZoneAPI.ListNetworkZones(ctx).Execute()
 	if err != nil {
-		return err
+		return fmt.Errorf("error listing network zones: %w", err)
 	}
+
+	allZones := networkZoneList
 
 	for resp.HasNextPage() {
-		var networkZoneSet []*okta.NetworkZone
-		resp, _ = resp.Next(ctx, &networkZoneSet)
-		output = append(output, networkZoneSet...)
+		var nextZoneSet []okta.ListNetworkZones200ResponseInner
+		resp, err = resp.Next(&nextZoneSet)
+		if err != nil {
+			return fmt.Errorf("error fetching next page of network zones: %w", err)
+		}
+		allZones = append(allZones, nextZoneSet...)
 	}
 
-	g.Resources = g.createResources(output)
+	g.Resources = g.createResources(allZones)
 	return nil
-}
-
-func attributesNetworkZone(networkZone *okta.NetworkZone) map[string]interface{} {
-	attributes := map[string]interface{}{}
-	attributes["usage"] = networkZone.Usage
-
-	if networkZone.Type == "DYNAMIC" {
-		if networkZone.Locations != nil {
-			attributes["dynamic_locations"] = networkZone.Locations
-		}
-	} else if networkZone.Type == "IP" {
-		switch {
-		case networkZone.Proxies != nil && networkZone.Gateways != nil:
-			attributes["proxies"] = networkZone.Proxies
-			attributes["gateways"] = networkZone.Gateways
-		case networkZone.Proxies != nil && networkZone.Gateways == nil:
-			attributes["proxies"] = networkZone.Proxies
-		case networkZone.Proxies == nil && networkZone.Gateways != nil:
-			attributes["gateways"] = networkZone.Gateways
-		}
-	}
-
-	return attributes
 }
